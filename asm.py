819d00a8d6f34eb43f9b69245af3e06d75e39144
Liran Nuna <liranuna@gmail.com>
2021-08-14 16:37:35 -0700

    Rudamentry parser: NOP, HLT, INX, DCX, LXI

---
+++ asm.py
@@ -0,0 +1,52 @@
+class ParseError(IOError):
+    pass
+
+
+def no_arg(name, value):
+    def encoder(arguments):
+        if len(arguments) != 0:
+            raise ParseError(f'instruction {name} takes no arguments')
+
+        return value
+
+    return encoder
+
+
+def high_4bit(name, mask):
+    register_map = {
+        'sp': 0,
+        'b': 1 << 4,
+        'd': 2 << 4,
+        'h': 3 << 4,
+    }
+
+    def encoder(arguments):
+        if len(arguments) != 1:
+            raise ParseError(f'instruction {name} takes one arguments')
+
+        register, = arguments
+        if register not in register_map:
+            raise ParseError(f'unknown register {register} for {name}')
+
+        return mask | register_map[register]
+
+    return encoder
+
+
+instruction_table = {
+    'nop': no_arg('nop', 0x00),
+    'hlt': no_arg('hlt', 0x01),
+    'inx': high_4bit('inx', 0x02),
+    'dcx': high_4bit('dcx', 0x03),
+    'lxi': high_4bit('inx', 0x04),
+}
+
+
+if __name__ == '__main__':
+    with open('test.as', 'rt') as f:
+        while line := f.readline():
+            instruction, _, arguments = line.partition(' ')
+            instruction = instruction.lower().strip()
+            arguments = list(filter(None, arguments.lower().strip().split(' ')))
+
+            print(hex(instruction_table[instruction](arguments)))
